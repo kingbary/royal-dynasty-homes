f2109da673b4c1839ef87b0868cf5cde85e2a6c4
kingbary <kingsleyakwa@gmail.com>
2025-09-21 12:55:45 +0100

    feat: setup routes, install fonts

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1>Our Projects</h1>
+      <p>Explore our diverse range of architectural projects that showcase our commitment to excellence.</p>
+    </div>
+  )
+}
