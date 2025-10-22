196c881cec00e8c658841c0c17459d0295e4d111
kingbary <kingsleyakwa@gmail.com>
2025-10-22 03:52:37 +0100

    chore: nav links hover state

--- components/universal/nav-bar.tsx
+++ components/universal/nav-bar.tsx
@@ -26,26 +26,29 @@ export default function NavBar() {
                     <Link href={'/'}>
                         <Logo />
                     </Link>
-                    <motion.ul className="flex flex-col space-x-2 md:space-x-8 md:flex-row">
+                    <motion.ul className="flex flex-col space-x-1 md:space-x-8 md:flex-row">
                         {
                             navLinks.map((link) => {
                                 return (
                                     <motion.li key={link.route || 'home'}
                                         initial={{ opacity: 0, x: 30 }}
                                         animate={{ opacity: 1, x: 0 }}
-                                        transition={{ duration: 0.6, delay: 0.4 }}>
+                                        transition={{ duration: 0.6, delay: 0.4 }}
+                                        className='group flex items-center'>
+                                        <span className='text-white opacity-0 group-hover:opacity-100'>{'['}</span>
                                         <Link
                                             href={link.route === '' ? '/' : `/${link.route}`}
-                                            className={`flex items-center gap-1 p-2.5 text-white transition-colors duration-200 hover:text-gray-300 ${isActive(link.route)
+                                            className={`flex items-center gap-1 px-1 py-2.5 text-white transition-colors duration-200 ${isActive(link.route)
                                                 ? ''
-                                                : 'hover:border-b hover:border-gray-300'
+                                                : ''
                                                 }`}
                                         >
                                             {isActive(link.route) && (
-                                                <div className='-ml-2 size-1.5 bg-white'></div>
+                                                <div className='-ml-2 size-1.5 bg-white group-hover:hidden'></div>
                                             )}
                                             {link.name}
                                         </Link>
+                                        <span className='text-white opacity-0 group-hover:opacity-100'>{']'}</span>
                                     </motion.li>
                                 )
                             })
@@ -53,13 +56,16 @@ export default function NavBar() {
                         <motion.li
                             initial={{ opacity: 0, x: 30 }}
                             animate={{ opacity: 1, x: 0 }}
-                            transition={{ duration: 0.6, delay: 0.4 }}>
+                            transition={{ duration: 0.6, delay: 0.4 }}
+                            className='group flex items-center'>
+                            <span className='text-white opacity-0 group-hover:opacity-100'>{'['}</span>
                             <button
                                 onClick={() => setIsOpen(true)}
-                                className='flex items-center gap-1 p-2.5 text-white transition-colors duration-200 hover:text-gray-300'
+                                className='flex items-center gap-1 px-1 py-2.5 text-white transition-colors duration-200'
                             >
                                 Contact Us
                             </button>
+                            <span className='text-white opacity-0 group-hover:opacity-100'>{']'}</span>
                         </motion.li>
                     </motion.ul>
                 </div>
