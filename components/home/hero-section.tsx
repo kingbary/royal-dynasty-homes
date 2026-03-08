cffb4d2383e0d637effeda268b578f11be04a157
kingbary <kingsleyakwa@gmail.com>
2026-03-08 07:33:39 +0100

    chore: comment out unused hero section slide

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -7,10 +7,10 @@ export default function HeroSection() {
     const [currentSlide, setCurrentSlide] = React.useState(0)
 
     const slides = [
-        {
-            image: "/images/hero-bg.png",
-            description: "GROUNDBREAKING EXECUTED BY: THE VICE CHANCELLOR PROFESSOR HAKEEM BABATUNDE FAWEHINMI"
-        },
+        // {
+        //     image: "/images/hero-bg.png",
+        //     description: "GROUNDBREAKING EXECUTED BY: THE VICE CHANCELLOR PROFESSOR HAKEEM BABATUNDE FAWEHINMI"
+        // },
         {
             image: "/images/building-img.webp",
             description: "The Royal Dynasty Homes merge unwavering architectural integrity with refined urban planning."
